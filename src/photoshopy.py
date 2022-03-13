fa182a2b50b10227254c5b37d52f37aad37333b6
tihbastos <Copobranco1>
2022-03-13 20:16:50 -0300

    Initial commit

---
+++ src/photoshopy.py
@@ -0,0 +1,79 @@
+import win32com.client
+import os
+from pathvalidate import sanitize_filename
+
+
+class Photoshopy:
+    app = None
+    psd_file = None
+
+    def __init__(self):
+        self.app = win32com.client.Dispatch("Photoshop.Application")
+        self.app.Visible = False
+
+    def closePhotoshop(self):
+        self.app.Quit()
+
+    def openPSD(self, filename):
+        if os.path.isfile(filename) == False:
+            self.closePhotoshop()
+            return False
+
+        self.app.Open(filename)
+        self.psd_file = self.app.Application.ActiveDocument
+        return True
+
+    def closePSD(self):
+        if self.psd_file is None:
+            raise Exception(FileNotFoundError)
+
+        self.app.Application.ActiveDocument.Close(2)
+
+    def update_layer_text(self, layer_name, text):
+        if not self.psd_file:
+            raise Exception(FileNotFoundError)
+        layer = self.psd_file.ArtLayers[layer_name]
+        layer_text = layer.TextItem
+        layer_text.contents = text
+        return True
+
+    def update_layer_image(self, layer_name, full_path_image):
+        if not self.psd_file:
+            raise Exception(FileNotFoundError)
+        layer = self.psd_file.ArtLayers[layer_name]
+        self.psd_file.ActiveLayer = layer
+
+        # edit content command - open psb file
+        type_id = self.app.Application.StringIDToTypeID("placedLayerEditContents")
+        action_descriptor = win32com.client.Dispatch("Photoshop.ActionDescriptor")
+        self.app.Application.ExecuteAction(type_id, action_descriptor, 1)
+
+        # replace content
+        type_id = self.app.Application.StringIDToTypeID("placedLayerReplaceContents")
+        id_null = self.app.Application.CharIDToTypeID("null")
+        action_descriptor = win32com.client.Dispatch("Photoshop.ActionDescriptor")
+        action_descriptor.PutPath(id_null, full_path_image)
+        idPgNm = self.app.Application.CharIDToTypeID("PgNm")
+        action_descriptor.PutInteger(idPgNm, 1)
+        self.app.Application.ExecuteAction(type_id, action_descriptor, 2)
+
+        # close psb file
+        self.app.Application.ActiveDocument.Close(1)
+
+        return True
+
+    def exportJPEG(self, filename, folder='', quality=12):
+        if not self.psd_file:
+            raise Exception(FileNotFoundError)
+        filename = sanitize_filename(filename)
+        full_path = os.path.join(folder, filename)
+
+        options = win32com.client.Dispatch("Photoshop.ExportOptionsSaveForWeb")
+        # jpg
+        options.Format = 6
+        options.Quality = quality
+
+        self.psd_file.Export(ExportIn=full_path, ExportAs=2, Options=options)
+
+        return os.path.isfile(full_path)
+
---
+++ tests/test_photoshopy.py
@@ -0,0 +1,72 @@
+import sys, os
+import unittest
+from photoshopy import Photoshopy
+
+
+class TestPhotoshopy(unittest.TestCase):
+    def setUp(self) -> None:
+        self.psd_origin = os.path.abspath("./resources/my_tests.psd")
+        self.jpeg_path = os.path.abspath("./resources/tmp")
+        self.jpeg_name = "my_tests.jpg"
+        self.jpeg_full_path = os.path.join(self.jpeg_path, self.jpeg_name)
+        self.image_to_replace_full_path = os.path.abspath("./resources/chris.jpg")
+
+        if not os.path.exists(self.jpeg_path):
+            os.mkdir(self.jpeg_path)
+
+        self.app = Photoshopy()
+
+    def tearDown(self) -> None:
+        self.app.closePhotoshop()
+
+        if os.path.exists(self.jpeg_full_path):
+            os.remove(self.jpeg_full_path)
+
+        if os.path.exists(self.jpeg_path):
+            os.rmdir(self.jpeg_path)
+
+    def test_OpenPSD(self):
+        opened = self.app.openPSD(self.psd_origin)
+        if opened:
+            self.app.closePSD()
+        self.assertTrue(opened)
+
+    def test_update_layer_text(self):
+        updated_1 = False
+        updated_2 = False
+
+        opened = self.app.openPSD(self.psd_origin)
+        if opened:
+            updated_1 = self.app.update_layer_text('Texto 1', 'Tiago')
+            updated_2 = self.app.update_layer_text('Texto 2', 'Bastos')
+            self.app.closePSD()
+
+        self.assertTrue(updated_1)
+        self.assertTrue(updated_2)
+
+    def test_exportJPEG(self):
+        exported = False
+
+        opened = self.app.openPSD(self.psd_origin)
+        if opened:
+            self.app.update_layer_text('Texto 1', 'Tiago')
+            self.app.update_layer_text('Texto 2', 'Bastos')
+
+            exported = self.app.exportJPEG(self.jpeg_name, self.jpeg_path)
+            self.app.closePSD()
+
+        self.assertTrue(exported)
+
+    def test_update_layer_image(self):
+        updated = False
+
+        opened = self.app.openPSD(self.psd_origin)
+        if opened:
+            updated = self.app.update_layer_image('foto', self.image_to_replace_full_path)
+            self.app.closePSD()
+
+        self.assertTrue(updated)
+
+
+if __name__ == '__main__':
+    unittest.main()
